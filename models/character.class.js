229544501fb2cd2380ae4f71517b1a1994d93da8
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2024-03-30 15:20:07 -0400

    animated character and chicken

--- models/character.class.js
+++ models/character.class.js
@@ -3,11 +3,31 @@ class Character extends MovableObject {
     height = 280;
     width = 150;
     y = 155;
-
-
+    IMAGES_WALKING = [
+        'img_pollo_locco/img/2_character_pepe/2_walk/W-21.png',
+        'img_pollo_locco/img/2_character_pepe/2_walk/W-22.png',
+        'img_pollo_locco/img/2_character_pepe/2_walk/W-23.png',
+        'img_pollo_locco/img/2_character_pepe/2_walk/W-24.png',
+        'img_pollo_locco/img/2_character_pepe/2_walk/W-25.png',
+        'img_pollo_locco/img/2_character_pepe/2_walk/W-26.png'
+    ];
+    
 
     constructor() {
         super().loadImage('img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate() {
+
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
     }
 
     jump() {
--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -4,10 +4,23 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.15;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(array) {
+        array.forEach((path) => {
+
+
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+    });
     }
 
     moveRight() {
@@ -15,7 +28,9 @@ class MovableObject {
     }
 
     moveLeft() {
-
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
     }
 
 }
